bbc6ba50ac3e0fcd8bf10c24bf94f78e2b1bbc6f
Asuten6418 <darkword2234@gmail.com>
2025-07-17 17:34:05 +0700

    Create Note.java

---
+++ app/src/main/java/com/example/myapplication/Note.java
@@ -0,0 +1,16 @@
+package com.example.sdfgh;
+
+public class Note {
+    //Attribute
+    String title;
+    String content;
+    String Date;
+
+    //Method
+    Void getSummary(){
+        System.out.println(title);
+        System.out.println(content);
+        System.out.println(Date);
+        return null;
+    }
+}
